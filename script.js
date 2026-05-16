dd7d14ad3d7cb05d5ff314c8e406b72b2f2de3c7
tgm-anaconda <tomg.m@icloud.com>
2026-05-16 21:49:23 +0200

    script.js

--- script.js
+++ script.js
@@ -2127,6 +2127,8 @@ function initVariantBot() {
           document.body.style.position = 'fixed';
           document.body.style.top = `-${scrollY}px`;
           document.body.style.width = '100%';
+          document.body.style.overflow = 'hidden';
+          document.documentElement.style.overflow = 'hidden';
           document.body._lockedScrollY = scrollY;
 
           // Bot: fullscreen, top:0, Höhe zunächst voller Bildschirm
@@ -2166,6 +2168,8 @@ function initVariantBot() {
           document.body.style.position = '';
           document.body.style.top = '';
           document.body.style.width = '';
+          document.body.style.overflow = '';
+          document.documentElement.style.overflow = '';
           window.scrollTo(0, savedY);
 
           // Bot zurücksetzen
